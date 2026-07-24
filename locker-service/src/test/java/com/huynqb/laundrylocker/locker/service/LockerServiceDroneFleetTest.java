6534599c8756336fdee8544bbbb3cbb2c82890e7
LDKhangg <lekhangmc12@gmail.com>
2026-07-25 05:50:35 +0700

    fix(locker): suppress UnnecessaryStubbingException in drone fleet tests
    Add @MockitoSettings(strictness = Strictness.LENIENT) to make stubs
    in setUp() harmless for test methods that don't exercise them.

--- locker-service/src/test/java/com/huynqb/laundrylocker/locker/service/LockerServiceDroneFleetTest.java
+++ locker-service/src/test/java/com/huynqb/laundrylocker/locker/service/LockerServiceDroneFleetTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import java.util.Optional;
@@ -27,6 +29,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class LockerServiceDroneFleetTest {
 
     @Mock
